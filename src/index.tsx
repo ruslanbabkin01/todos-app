d6d78307d9dbf9952411edc8faac9e1fd9440dda
Ruslan Babkin <ruslan.babkin01@gmail.com>
2023-01-11 14:53:15 +0200

    del basename

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/todo-app">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
